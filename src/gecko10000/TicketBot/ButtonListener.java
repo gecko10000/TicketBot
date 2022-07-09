378a7e63792cf3c787ce079ba3ecebbe28b2b3bb
gecko10000 <levtey@gmail.com>
2022-07-09 02:04:52 -0700

    Fix button press acknowledgement

--- src/gecko10000/TicketBot/ButtonListener.java
+++ src/gecko10000/TicketBot/ButtonListener.java
@@ -1,12 +1,8 @@
 package gecko10000.TicketBot;
 
-import discord4j.common.util.Snowflake;
-import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
-import discord4j.core.spec.MessageCreateSpec;
-import discord4j.core.spec.TextChannelCreateSpec;
-import gecko10000.TicketBot.utils.Config;
-import reactor.core.publisher.Flux;
+import discord4j.core.object.entity.Guild;
+import reactor.core.publisher.Mono;
 
 public class ButtonListener {
 
@@ -14,11 +10,12 @@ public class ButtonListener {
 
     public ButtonListener(TicketBot bot) {
         this.bot = bot;
-        Flux<ButtonInteractionEvent> eventFlux = bot.client.on(ButtonInteractionEvent.class)
-                .filter(e -> e.getCustomId().equals("ticket"));
-        eventFlux.flatMap(e -> e.getInteraction().getGuild())
-                .zipWith(eventFlux.map(e -> e.getInteraction().getUser()))
-                .subscribe(guildUserTuple -> bot.ticketManager.openTicket(guildUserTuple.getT1(), guildUserTuple.getT2()));
+        bot.client.on(ButtonInteractionEvent.class, e -> {
+            if (!e.getCustomId().equals("ticket")) return Mono.empty();
+            Mono<Guild> guildMono = e.getInteraction().getGuild();
+            guildMono.subscribe(guild -> bot.ticketManager.openTicket(guild, e.getInteraction().getUser()));
+            return e.reply();
+        }).subscribe();
     }
 
 }
